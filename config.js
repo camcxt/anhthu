a04e6f5d31900957ad770b538e89e47df35501a8
camcxt <83831615+camcxt@users.noreply.github.com>
2021-08-12 13:03:22 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Cô ơi',
+    introDesc: `T có điều muốn hỏi cô nhớ phải trả lời Thật lòng đấy!`,
+    btnIntro: 'oke',
+    title: 'Cô có điều muốn kể với tôi phải không 🙄',
+    desc: 'Cô không trả lời hoặc thoát là cô có chuyện muốn kể cho t rồi',
+    btnYes: 'Ò🙄',
+    btnNo: 'ko có 🙄',
+    question:'cô ko cần nói gì cả',
+    btnReply: 'ok 🙄',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Tôi biết mà hhh 🙄🙄🙄 ',
+    messDesc: 'Cô có thể nhắn tin cho tôi bất cứ lúc nào tôi luôn ở đây he',
+    btnAccept: 'Ò🙄',
+    messLink: 'https://www.facebook.com/camcxt/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
